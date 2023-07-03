70c3583bc705293adc0fdcda3a4a99d0084d60a9
bonbongiraffe <francescowai@yahoo.com>
2023-07-03 15:17:01 -0400

    first attempt

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    badges = [badge_maker(name) for name in names]
+    return badges
+
+# auxiliary function
+def welcome_message (name, room_num):
+    return (f"Hello, {name}! You'll be assigned to room {room_num}!")
 
 def assign_rooms(names):
-    return None
+    room = 1 
+    welcome_messages = []
+    for name in names :
+        welcome_messages.append(welcome_message(name, room))
+        room += 1
+    return welcome_messages
 
 def printer(names):
-    return None
+    messages = assign_rooms(names)
+    for name in names :
+        print (badge_maker(name))
+    for message in messages :
+        print (message)
+
